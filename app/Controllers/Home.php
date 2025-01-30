3aeb136d6c2fd06bfa7c62560812c743b145ba41
aleaguilarc4 <aleaguilarc4@gmail.com>
2025-01-30 18:18:49 +0100

    primera prueba

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,9 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+
+    public function prueba(): string
+    {
+        return view('prueba_mensaje');
+    }
 }
--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -240,7 +240,7 @@
     <h1>About this page</h1>
 
     <p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
+    <p>prueba rama feature.</p>
     <p>If you would like to edit this page you will find it located at:</p>
 
     <pre><code>app/Views/welcome_message.php</code></pre>
